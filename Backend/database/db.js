9fbd71a902e7faa66954af1bc50db70e08fb7309
Harshita-Naik16 <harshitanaikwebdev@gmail.com>
2024-07-04 12:06:25 +0530

    add: signup

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://system:system@cluster0.4sl5ce8.mongodb.net/system-db`;
-    try {
-        await mongoose.connect(URL, { useNewUrlParser: true })
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.log('Error while connecting to the database ', error);
-    }
+  const URL = `mongodb+srv://amanprajapati7266:7xH1cHTT8hf8x94l@cluster0.cqolvlp.mongodb.net/Blogwebsite`;
+  try {
+    await mongoose.connect(URL, { useNewUrlParser: true });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error while connecting to the database ", error);
+  }
 };
 
 export default Connection;
--- Frontend/src/pages/admin/adminLogin/Signup.jsx
+++ Frontend/src/pages/admin/adminLogin/Signup.jsx
@@ -1,202 +1,216 @@
 import React, { useState } from "react";
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Input,
-    Button,
-    Typography,
+  Card,
+  CardHeader,
+  CardBody,
+  Input,
+  Button,
+  Typography,
 } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { baseUrl } from "../../../../config";
 
 export default function Signup() {
-    const navigate = useNavigate();
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const signup = async () => {
-        if (!firstName || !lastName || !email || !password || !confirmPassword) {
-            return toast.error("All fields are required");
-        }
+  const signup = async () => {
+    if (!firstName || !email || !password || !confirmPassword) {
+      return toast.error("All fields are required");
+    }
 
-        if (password !== confirmPassword) {
-            return toast.error("Passwords do not match");
-        }
+    if (password !== confirmPassword) {
+      return toast.error("Passwords do not match");
+    }
 
-        const profileData = {
-            firstName,
-            lastName,
-            email,
-            profilePicture: null,
-        };
-
-        localStorage.setItem("profileData", JSON.stringify(profileData));
-        toast.success("Signup successful");
-
-        navigate('/profile');
+    const profileData = {
+      username: firstName,
+      email: email,
+      password: password,
     };
 
-    const goToAdminLogin = () => {
-        navigate('/adminlogin');
-    };
+    try {
+      const response = await fetch(`${baseUrl}/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(profileData),
+      });
 
-    const handleGoogleSignup = () => {
-        toast.success("Google Signup successful");
-        navigate('/profile');
-    };
+      const res = response.json();
 
-    const goToHome = () => {
-        navigate('/');
-    };
+      console.log(res);
+      //   if (response.ok) {
+      //     navigate("/profile");
+      //   }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const cardStyle = {
-        background: '#dcdcdc',
-        borderRadius: '8px',
-        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-        width: '90%',
-        maxWidth: '600px',
-    };
+  const goToAdminLogin = () => {
+    navigate("/adminlogin");
+  };
 
-    const headerStyle = {
-        background: '#1d2951',
-        color: 'white',
-    };
+  const handleGoogleSignup = () => {
+    toast.success("Google Signup successful");
+    navigate("/profile");
+  };
 
-    const textStyle = {
-        color: '#333',
-    };
+  const goToHome = () => {
+    navigate("/");
+  };
 
-    const buttonStyle = {
-        background: '#007BFF',
-        color: 'white',
-        padding: '10px 20px',
-        borderRadius: '5px',
-        cursor: 'pointer',
-        marginTop: '10px',
-        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-    };
+  const cardStyle = {
+    background: "#dcdcdc",
+    borderRadius: "8px",
+    boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+    width: "90%",
+    maxWidth: "600px",
+  };
 
-    const homeButtonStyle = {
-        position: 'absolute',
-        top: '20px',
-        right: '20px',
-        background: '#FF5722',
-        color: 'white',
-        padding: '10px 20px',
-        borderRadius: '5px',
-        cursor: 'pointer',
-        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-    };
+  const headerStyle = {
+    background: "#1d2951",
+    color: "white",
+  };
 
-    return (
-        <div className="flex justify-center items-center h-screen relative" style={{ backgroundColor: 'white' }}>
-            <Button
-                onClick={goToHome}
-                style={homeButtonStyle}
-            >
-                Home
+  const textStyle = {
+    color: "#333",
+  };
+
+  const buttonStyle = {
+    background: "#007BFF",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    marginTop: "10px",
+    boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+  };
+
+  const homeButtonStyle = {
+    position: "absolute",
+    top: "20px",
+    right: "20px",
+    background: "#FF5722",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+  };
+
+  return (
+    <div
+      className="flex justify-center items-center h-screen relative"
+      style={{ backgroundColor: "white" }}
+    >
+      <Button onClick={goToHome} style={homeButtonStyle}>
+        Home
+      </Button>
+      <Card style={cardStyle}>
+        <CardHeader
+          color="blue"
+          floated={false}
+          shadow={false}
+          className="m-0 grid place-items-center rounded-b-none py-8 px-4 text-center"
+          style={headerStyle}
+        >
+          <Typography variant="h4" style={{ color: "white" }}>
+            Sign Up
+          </Typography>
+        </CardHeader>
+        <CardBody>
+          <form className="flex flex-col gap-4">
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-700">
+                First Name<span className="text-red-500">*</span>
+              </label>
+              <Input
+                type="text"
+                placeholder="Enter your first name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                // required
+                size="lg"
+              />
+            </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-700">
+                Last Name<span className="text-red-500">*</span>
+              </label>
+              <Input
+                type="text"
+                placeholder="Enter your last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                // required
+                size="lg"
+              />
+            </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-700">
+                Email<span className="text-red-500">*</span>
+              </label>
+              <Input
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                // required
+                size="lg"
+              />
+            </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-700">
+                Password<span className="text-red-500">*</span>
+              </label>
+              <Input
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                // required
+                size="lg"
+              />
+            </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-700">
+                Confirm Password<span className="text-red-500">*</span>
+              </label>
+              <Input
+                type="password"
+                placeholder="Enter your confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                // required
+                size="lg"
+              />
+            </div>
+            <Button onClick={signup} style={buttonStyle}>
+              Sign Up
             </Button>
-            <Card style={cardStyle}>
-                <CardHeader
-                    color="blue"
-                    floated={false}
-                    shadow={false}
-                    className="m-0 grid place-items-center rounded-b-none py-8 px-4 text-center"
-                    style={headerStyle}
-                >
-                    <Typography variant="h4" style={{ color: 'white' }}>
-                        Sign Up
-                    </Typography>
-                </CardHeader>
-                <CardBody>
-                    <form className="flex flex-col gap-4">
-                        <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-700">
-                                First Name<span className="text-red-500">*</span>
-                            </label>
-                            <Input
-                                type="text"
-                                placeholder="Enter your first name"
-                                value={firstName}
-                                onChange={(e) => setFirstName(e.target.value)}
-                                // required
-                                size="lg"
-                            />
-                        </div>
-                        <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-700">
-                                Last Name<span className="text-red-500">*</span>
-                            </label>
-                            <Input
-                                type="text"
-                                placeholder="Enter your last name"
-                                value={lastName}
-                                onChange={(e) => setLastName(e.target.value)}
-                                // required
-                                size="lg"
-                            />
-                        </div>
-                        <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-700">
-                                Email<span className="text-red-500">*</span>
-                            </label>
-                            <Input
-                                type="email"
-                                placeholder="Enter your email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                // required
-                                size="lg"
-                            />
-                        </div>
-                        <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-700">
-                                Password<span className="text-red-500">*</span>
-                            </label>
-                            <Input
-                                type="password"
-                                placeholder="Enter your password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                // required
-                                size="lg"
-                            />
-                        </div>
-                        <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-700">
-                                Confirm Password<span className="text-red-500">*</span>
-                            </label>
-                            <Input
-                                type="password"
-                                placeholder="Enter your confirm password"
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                // required
-                                size="lg"
-                            />
-                        </div>
-                        <Button onClick={signup} style={buttonStyle}>
-                            Sign Up
-                        </Button>
-                        <Button
-                            onClick={goToAdminLogin}
-                            style={{ ...buttonStyle, backgroundColor: '#4CAF50' }} // Green background
-                        >
-                            Admin Login
-                        </Button>
-                        <Button
-                            onClick={handleGoogleSignup}
-                            style={{ ...buttonStyle, backgroundColor: '#DB4437' }} // Google red background
-                        >
-                            Sign Up with Google
-                        </Button>
-                    </form>
-                </CardBody>
-            </Card>
-        </div>
-    );
+            <Button
+              onClick={goToAdminLogin}
+              style={{ ...buttonStyle, backgroundColor: "#4CAF50" }} // Green background
+            >
+              Admin Login
+            </Button>
+            <Button
+              onClick={handleGoogleSignup}
+              style={{ ...buttonStyle, backgroundColor: "#DB4437" }} // Google red background
+            >
+              Sign Up with Google
+            </Button>
+          </form>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
